c083b22491127375bc050cf1e6cc877438c0522d
Juan Pablo CD <jpcastiglioni@gmail.com>
2021-07-31 12:28:16 -0300

    Added cabecera, formulario, ingreso and egreso components (empty)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CabeceroComponent } from './cabecero/cabecero.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { IngresoComponent } from './ingreso/ingreso.component';
+import { EgresoComponent } from './egreso/egreso.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceroComponent,
+    FormularioComponent,
+    IngresoComponent,
+    EgresoComponent,
   ],
   imports: [
     BrowserModule
